a637eac9ddbc7ad7ed9423f346a1c3d8e5ce71cd
raman <bayramofframan0@gmail.com>
2023-08-19 02:13:15 +0400

    lesson_5 tasks are ready

---
+++ javascript/js_lesson_4_OPERATORS_IF/app.js
@@ -0,0 +1,66 @@
+// Task 1
+// ******
+
+/* let firstName = prompt('type your name');
+let lastName = prompt('type your surname')
+
+console.log(`${firstName} ${lastName}`) */
+
+// *******************************************************************************
+
+//Task 2
+// *******
+
+// let firstName = prompt('type your name')
+// console.log(`${firstName} -> ~ ~ ${firstName}`)
+
+// *******************************************************************************
+// Task 3
+// *******
+
+// const LENGTH = 9
+// const WIDE = 3
+
+// const PERIMETER = (LENGTH + WIDE) *2
+// console.log(PERIMETER)
+
+// *********************************************************************************
+
+// TASK_4
+// *********
+
+// let secdiyimMusiqi = prompt(`sevdiyin musiqinin adini yaz:
+// 1.wavin' flag
+// 2.temptation arash
+// 3.broken angle
+// 4.aynadan gozel(kio kio)
+// `)
+
+// if(secdiyimMusiqi === '1'){
+//  console.log(`Give me  freedom,
+//  Give me fire,
+//  Give me reason,
+//  Take me higher.
+//  `)
+// }else if(secdiyimMusiqi === '2'){
+//  console.log(`Cix yasil duze,
+//  Qonaq gel bize,
+//  Tez gel ey gulum
+//  `)
+// }else if(secdiyimMusiqi === '3'){
+//  console.log(`Saçın ucun horməzlər,
+// Gülü sulu dərməzlər
+// Sarı gəlin`)
+// }
+// else if(secdiyimMusiqi === '4'){
+//  console.log(`
+//  Aynadan gozel aynadan,
+//  Bal dadan gozel bal dadan
+// `)
+// }
+
+
+// ******************************************
+// JavaScript TASKS
+ let ed = +prompt('ededi yazin', 49);
+
