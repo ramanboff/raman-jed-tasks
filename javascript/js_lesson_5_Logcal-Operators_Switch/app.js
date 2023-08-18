0088b438f3ba32af7bacfcbbb58974e8bb4065c0
raman <bayramofframan0@gmail.com>
2023-08-19 01:49:08 +0400

    lesson_5 Task is ready.

---
+++ javascript/js_lesson_5_Logcal-Operators_Switch/app.js
@@ -0,0 +1,40 @@
+// Task1
+
+let direction = prompt('istiqameti daxil et:')
+
+switch(direction) {
+  case 'up':
+    console.log(' yuxari get')
+break;
+  case 'right':
+    console.log( 'saga get')
+    break;
+  case 'left':
+    console.log( 'sola get')
+    break;
+  case 'down':
+    console.log( 'awagi get')
+    break;
+  default:
+    console.log('terpenme')
+}
+
+// *********************************************************************
+
+// TASK-2
+
+let ceki = +prompt('cekinizi yazin:')
+let boy  = +prompt('boyunuz nece metrdir?')
+
+let result = Math.round(ceki /(boy * boy))
+console.log(result)
+
+if(result < 18.5){
+  console.log('biraz yemek ye')
+}else if(result < 25){
+  console.log('Normalsan')
+}else if(result < 30){
+  console.log('BMI-in kokdur')
+}else{
+  console.log(`'obese'`)
+}
