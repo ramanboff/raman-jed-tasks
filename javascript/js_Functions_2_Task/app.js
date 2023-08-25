0dd203ae0b4bc6f16641d9ae1753665af152490c
raman <bayramofframan0@gmail.com>
2023-08-25 20:41:07 +0400

    Task_1_isActive done

---
+++ javascript/js_Functions_2_Task/app.js
@@ -0,0 +1,11 @@
+//*isActive TASK_1
+function isActive(username){
+return `${username} is active now`
+}
+
+console.log(isActive('John'))
+console.log(isActive('Kamil'))
+console.log(isActive('Akif'))
+
+////////////////////////////////////////////////
+
