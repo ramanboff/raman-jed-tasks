76fc61175b893a0a3427f06047c2b5f52cf78f22
raman <bayramofframan0@gmail.com>
2023-08-25 23:00:44 +0400

    task_2  and task_3 done

--- javascript/js_Functions_2_Task/app.js
+++ javascript/js_Functions_2_Task/app.js
@@ -1,11 +1,40 @@
 //*isActive TASK_1
-function isActive(username){
-return `${username} is active now`
-}
+// function isActive(username){
+// return `${username} is active now`
+// }
 
-console.log(isActive('John'))
-console.log(isActive('Kamil'))
-console.log(isActive('Akif'))
+// console.log(isActive('John'))
+// console.log(isActive('Kamil'))
+// console.log(isActive('Akif'))
 
 ////////////////////////////////////////////////
+//*TASK_2   // 3 ededden en boyuyunu tapan funksiya
 
+// function largeNum(num1, num2, num3){
+//   if( num1> num2 && num1> num3 ){
+//    return num1
+//   }else if(num2>num1 &&  num2> num3){
+//    return num2
+//   }else{
+//    return num3
+//   }
+
+//  }
+
+//  console.log(largeNum(11,2,33))
+//  console.log(largeNum(3,9,5))
+//  console.log(largeNum(9,6,16))
+//////////////////////////////////////////////////////////////////
+
+//* Rubl TASK
+
+let dollar = +prompt('dollar hesabinizda ne qede var: ?')
+let ruble = +prompt('rubl hasabinizda ne qede var ?')
+
+function toRuble(rub, dol) {
+  let sum = ''
+  dol = 75 * rub
+  sum += `Butun depozitler uzre mebleg ${rub + dol} rubl `
+  return sum
+}
+console.log(toRuble(ruble, dollar))
