a1d0c5a5dd1f4cec06725c53a0ae7793b090084e
ramanboff <bayramofframan@gmail.com>
2023-09-12 23:29:35 +0400

    Task_3

---
+++ javascript/js_ES6_Array_Methods_12_sep_2023/Task_3/app.js
@@ -0,0 +1,14 @@
+//*Task3. Get an array of objects containing only the name and email of users whose name starts with 'C'.
+const url = "https://jsonplaceholder.typicode.com/users";
+const ES6ArrayMethods = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((users) => {
+      const startsWithC = users
+        .filter((user) => user.name.startsWith("C"))
+        .map((user) => `name: ${user.name} __  email: ${user.email}`);
+      console.log(startsWithC);
+    });
+};
+
+console.log(ES6ArrayMethods(url));
