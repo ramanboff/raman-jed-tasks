edb5b82c73c7ce61606e840bfb9eb29ce3ff5d27
ramanboff <bayramofframan@gmail.com>
2023-09-13 02:04:02 +0400

    Task_11

---
+++ javascript/js_ES6_Array_Methods_12_sep_2023/Task_11/app.js
@@ -0,0 +1,13 @@
+//*Task11. Get an array of users whose phone number starts with the area code '1-770'.
+
+const ArrayMethods = (url) => {
+ fetch(url)
+ .then((response) => response.json())
+ .then((users) => {
+  const PhoneStartsWith1_770 = users.filter((user) => user.phone.startsWith('1-770'))
+  console.log(PhoneStartsWith1_770);
+ })
+
+
+}
+console.log(ArrayMethods('https://jsonplaceholder.typicode.com/users'));
