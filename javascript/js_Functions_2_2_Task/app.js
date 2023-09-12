04dfef9bf5b35c64bf0ee36694577c919a183b65
ramanboff <bayramofframan@gmail.com>
2023-09-12 22:12:16 +0400

    js function_2_2

---
+++ javascript/js_Functions_2_2_Task/app.js
@@ -0,0 +1,14 @@
+//*TASK_1
+// Bir add funksiyasi yazin. 3 arqument alsin. ilk 2si ededler (eded1 ve eded2) 3cusu ise, funksiya olsun. (callback). men o funksiyaya console.log ve ya alert verib, ozum teyin edim, neticeni alert-de gostersin, yoxsa console-da.
+/* add(3,5,console.log) //  console-a 8 yazmalidi
+add(3,5,alert) */ //  alert-e 8 yazmalidi
+
+const add = (num1, num2, cb) => {
+  cb(num1 + num2)
+}
+add(3, 5, function (result) {
+  alert(`result: ${result}`)
+  console.log(`result: ${result}`)
+})
+//////////////////////////////////////////////////////////////////
+//*TASK_2
