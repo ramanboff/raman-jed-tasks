43013ba35ad4d82c2c783c1b0a7800be6d94c11e
ramanboff <bayramofframan@gmail.com>
2023-09-13 10:33:44 +0400

    Task_15

---
+++ javascript/js_ES6_Array_Methods_12_sep_2023/Task_14/Task_15/app.js
@@ -0,0 +1,17 @@
+//*Task15. Get the average length of all user names.
+
+let url = "https://jsonplaceholder.typicode.com/users";
+
+const ES6ArrayMethods = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((users) => {
+      let getLengthOfNames = users.map((user) => user.name.length)
+      let getSumOfLength = getLengthOfNames.reduce((acc, curr) => acc+curr)
+      getSumOfLength /= users.length
+      console.log(getSumOfLength);
+
+      
+    });
+};
+console.log(ES6ArrayMethods(url));
