c4f5e0561b46f70ca85da5d87b06cee476c78473
ramanboff <bayramofframan@gmail.com>
2023-09-13 09:53:07 +0400

    Task_13

--- javascript/js_ES6_Array_Methods_12_sep_2023/Task_13/app.js
+++ javascript/js_ES6_Array_Methods_12_sep_2023/Task_13/app.js
@@ -7,7 +7,7 @@ const ES6ArrayMethods = (url) => {
     .then((res) => res.json())
     .then((users) => {
       const GetAllUserCities = users.map((user) => user.address.city );
-      console.log(GetAllUserCities);
+      console.log(GetAllUserCities.join('\n'));
     });
 };
 console.log(ES6ArrayMethods(url));
--- javascript/js_ES6_Array_Methods_12_sep_2023/Task_7_Alinmadi/app.js
+++ javascript/js_ES6_Array_Methods_12_sep_2023/Task_7_Alinmadi/app.js
@@ -8,8 +8,10 @@ const ES6ArrayMethods = (url) => {
   fetch(url)
     .then((res) => res.json())
     .then((users) => {
-      const getUserWithHighestId = users.filter((user,i, arr) => user.id === arr[i].id  );
-      console.log(getUserWithHighestId);
+      // const getUserWithHighestId = users.filter((user,i, arr) => user.id === arr[i].id );
+      // console.log(getUserWithHighestId);
+      const netice = users.sort((a,b) => b.id - a.id)[0]
+      console.log(netice);
     });
 };
 console.log(ES6ArrayMethods(url));
