bd89b5af5e5d4ce368fdbba15ab4acfa17cdfe02
ramanboff <bayramofframan@gmail.com>
2023-09-13 11:13:09 +0400

    Task_15

--- javascript/js_ES6_Array_Methods_12_sep_2023/Task_15/app.js
+++ javascript/js_ES6_Array_Methods_12_sep_2023/Task_15/app.js
@@ -9,7 +9,7 @@ const ES6ArrayMethods = (url) => {
       let getLengthOfNames = users.map((user) => user.name.length)
       let getSumOfLength = getLengthOfNames.reduce((acc, curr) => acc+curr)
       getSumOfLength /= users.length
-      console.log(getSumOfLength);
+      console.log(getSumOfLength);  
 
       
     });
